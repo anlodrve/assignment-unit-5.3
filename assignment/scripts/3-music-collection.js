70f6da2732f3d1b59b7c2df1831ae71e315e7001
Andrea Love <andrealovelovelove@gmail.com>
2023-01-11 15:40:41 -0600

    created array, function, and added 6 albums to collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,20 @@
 console.log('***** Music Collection *****')
+let collection = [];
+function addToCollection(title, artist, yearPublished){
+    let recordObject = {
+        title,
+        artist,
+        yearPublished
+    }
+    collection.push(recordObject);
+    return recordObject;
+}
+console.log(addToCollection("Blue", "Joni Mitchell", "1971"));
+console.log(addToCollection("Trio", "Dolly Parton, Emmylous Harris, Linda Ronstadt", "1987"));
+console.log(addToCollection("Heart Like A Wheel", "Linda Ronstadt", "1974"));
+console.log(addToCollection("Court and Spark","Joni Mitchell", "1974"));
+console.log(addToCollection("Renaissance", "Beyonce", "2022"));
+console.log(addToCollection("Lemonade", "Beyonce", "2016"));
+console.log(collection);
+
+
