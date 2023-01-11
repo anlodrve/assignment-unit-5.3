96b1c11f5491012e2bef52f30fdf2cf3eb2ff744
Andrea Love <andrealovelovelove@gmail.com>
2023-01-11 15:59:16 -0600

    created showCollection function and tested

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -10,11 +10,17 @@ function addToCollection(title, artist, yearPublished){
     return recordObject;
 }
 console.log(addToCollection("Blue", "Joni Mitchell", "1971"));
-console.log(addToCollection("Trio", "Dolly Parton, Emmylous Harris, Linda Ronstadt", "1987"));
+console.log(addToCollection("Trio", "Dolly Parton, Emmylou Harris, Linda Ronstadt", "1987"));
 console.log(addToCollection("Heart Like A Wheel", "Linda Ronstadt", "1974"));
 console.log(addToCollection("Court and Spark","Joni Mitchell", "1974"));
 console.log(addToCollection("Renaissance", "Beyonce", "2022"));
 console.log(addToCollection("Lemonade", "Beyonce", "2016"));
 console.log(collection);
 
-
+function showCollection(array){
+        console.log(array.length); 
+        for (recordObject of collection){
+            console.log(recordObject.title, 'by', recordObject.artist, 'published in', recordObject.yearPublished );
+        }
+}
+console.log(showCollection(collection));
