58c7310286bc4864d5d7df00fc306feadc68cd11
Andrea Love <andrealovelovelove@gmail.com>
2023-01-12 15:43:10 -0600

    got search function working

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,10 +1,11 @@
 console.log('***** Music Collection *****')
 let collection = [];
-function addToCollection(title, artist, yearPublished){
+function addToCollection(title, artist, yearPublished, tracks){
     let recordObject = {
         title,
         artist,
-        yearPublished
+        yearPublished,
+        tracks
     }
     collection.push(recordObject);
     return recordObject;
@@ -38,3 +39,49 @@ function findByArtist(artist){
 console.log(findByArtist("Joni Mitchell"));
 console.log(findByArtist("Beyonce"));
 console.log(findByArtist("The Beatles"));
+console.log(findByArtist("Linda Ronstadt"));
+
+//Stretch goals
+
+
+
+function search(searchObject, array){
+    console.log('in search function, searching for:', searchObject , 'in our collection');
+    if(Object.keys(searchObject).length === 0){
+        return collection 
+    }
+    let newArray = []
+    for(let recordObject of collection){
+        if(searchObject.artist === recordObject.artist && searchObject.year === recordObject.yearPublished){
+            newArray.push(recordObject)
+            } //end if statement
+        } //end for loop 
+        return newArray
+    }
+   
+let searchObject = {
+    artist: 'Joni Mitchell',
+    year: '1971'
+}
+console.log(search(searchObject, collection));
+
+searchObject = {
+
+}
+console.log(search(searchObject, collection))
+
+searchObject = {
+    artist: 'Joni Mitchell',
+    year: '1978'
+}
+console.log(search(searchObject, collection))
+
+searchObject = {
+    artist: 'Linda Ronstadt',
+    year: '1974'
+}
+console.log(search(searchObject, collection));
+
+//output needs -a new array of item in collection matching all of the search criteria 
+//             -if no results, return empty
+//             -
